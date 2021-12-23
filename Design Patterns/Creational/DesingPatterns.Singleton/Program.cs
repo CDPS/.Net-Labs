fc196844c3cfc689bab0a1cecd02f12ebb983327
Cristian_Palechor <Cristian_Palechor@EPCOBOGW0356>
2021-12-23 09:36:54 -0500

    Implementation of Singleton Desing Pattern

---
+++ Design Patterns/Creational/DesingPatterns.Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesingPatterns.Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Singleton.Instance;
+            Console.WriteLine(result.Id);
+        }
+    }
+}
---
+++ Design Patterns/Creational/DesingPatterns.Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DesingPatterns.Singleton
+{
+    public sealed class Singleton
+    {
+        // reading this will initialize the instance
+        public static readonly Lazy<Singleton> _lazy = new Lazy<Singleton>(() => new Singleton());
+        public string Id { get; set; }
+        public static Singleton Instance
+        {
+            get
+            {
+                return _lazy.Value;
+            }
+        }
+
+        private Singleton()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
