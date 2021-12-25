97cbc5d4f9b1dd3540c7e11bd4400424f3cfba37
Cristian_Palechor <Cristian_Palechor@EPCOBOGW0356>
2021-12-25 13:44:10 -0500

    Implementation of Command Desing Pattern

---
+++ Design Patterns/Behavioral/DesingPatterns.Command/Commands/ICommand.cs
@@ -0,0 +1,9 @@
+﻿namespace DesingPatterns.Command.Commands
+{
+    public interface ICommand
+    {
+        void Execute();
+        bool CanExecute();
+        void Undo();
+    }
+}
---
+++ Design Patterns/Behavioral/DesingPatterns.Command/Repositories/Products/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using DesingPatterns.Command.Models;
+using System.Collections.Generic;
+
+namespace DesingPatterns.Command.Repositories
+{
+    public interface IProductRepository
+    {
+        Product FindBy(string articleId);
+        int GetStockFor(string articleId);
+        IEnumerable<Product> All();
+        void DecreaseStockBy(string articleId, int amount);
+        void IncreaseStockBy(string articleId, int amount);
+    }
+}
