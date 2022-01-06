aa7218f77e56a5be8d0ef7c9cfff4b1cfc5bae18
Cristian_Palechor <Cristian_Palechor@EPCOBOGW0356>
2022-01-06 11:34:04 -0500

    Implement Composite Design Pattern

---
+++ Design Patterns/Structural/DesignPatterns.Composite/Model/FileItem.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace DesignPatterns.Composite.Model
+{
+    public class FileItem : FileSystemItem
+    {
+        public FileItem(string name, long fileBytes) : base(name)
+        {
+            this.FileBytes = fileBytes;
+        }
+
+        public long FileBytes { get; }
+
+        public override decimal GetSizeInKB()
+        {
+            return decimal.Divide(this.FileBytes, 1000);
+        }
+
+        public override void Print(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + this.Name);
+        }
+    }
+}
---
+++ Design Patterns/Structural/DesignPatterns.Composite/Model/FileSystemItem.cs
@@ -0,0 +1,17 @@
+﻿
+namespace DesignPatterns.Composite.Model
+{
+    public abstract class FileSystemItem
+    {
+        public FileSystemItem(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; }
+
+        public abstract decimal GetSizeInKB();
+
+        public abstract void Print(int depth);
+    }
+}
