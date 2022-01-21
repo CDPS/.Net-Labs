887cdb071f39efded3cdb6725fd0b97b9334536f
Cristian_Palechor <Cristian_Palechor@EPCOBOGW0356>
2022-01-21 17:15:19 -0500

    Remove unnecessary usings

--- ASP.NET/WebApi/Camp.API/Startup.cs
+++ ASP.NET/WebApi/Camp.API/Startup.cs
@@ -5,17 +5,10 @@ using Camp.Data.Repository.SpeakerRepository;
 using Camp.Data.Repository.TalkRepository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Camp.API
 {
