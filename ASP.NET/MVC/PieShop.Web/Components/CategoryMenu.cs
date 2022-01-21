327dde022b4e761d2548cdc1ecf7e7abbb235018
Cristian Daniel Palechor Sepulveda <cristian.palechor@masivian.com>
2022-01-20 20:27:16 -0500

    Add ShoppingCart and Categories View Components

---
+++ ASP.NET/MVC/PieShop.Web/Components/CategoryMenu.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Data.CategoryRepository;
+using System.Linq;
+
+namespace PieShop.Web.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.GetAll().OrderBy(c => c.CategoryName);
+            return View(categories);
+        }
+    }
+}
--- ASP.NET/MVC/PieShop.Web/Controllers/PieController.cs
+++ ASP.NET/MVC/PieShop.Web/Controllers/PieController.cs
@@ -1,34 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PieShop.Data.CategoryRepository;
 using PieShop.Data.PieRepository;
+using PieShop.Entiies.Pie;
 using PieShop.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PieShop.Web.Controllers
 {
     public class PieController : Controller
     {
-        private readonly IPieRepository _pieRepostory;
+        private readonly IPieRepository _pieRepository;
         private readonly ICategoryRepository _categoryRepository;
 
         public PieController(IPieRepository pieRepostory, ICategoryRepository categoryRepository)
         {
-            _pieRepostory = pieRepostory;
+            _pieRepository = pieRepostory;
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string category)
         {
-            PieListViewModel pieListViewModel = new PieListViewModel()
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
             {
-                Pies = _pieRepostory.GetAllPies(),
-                CurrentCategory = "Cheese cakes"
-            };
-            return View(pieListViewModel);
+                pies = _pieRepository.GetAll().OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.GetAll().Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.GetAll().FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
 
         public IActionResult Details(int id)
         {
-            var pie = _pieRepostory.GetPiesById(id);
+            var pie = _pieRepository.GetPiesById(id);
             if(pie == null)
                 return NotFound();
             return View(pie);
